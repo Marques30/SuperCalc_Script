9371523b9361c61443ed827a5afe2b94079b550d
Marques30 <marques.butilla@gmail.com>
2016-01-27 11:32:19 -0800

    It broke

--- TestCase2.py
+++ TestCase2.py
@@ -1,34 +1,79 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://jeremydomasian.github.io/supercalc_v1-4/SuperCalc-v1.html')
+def Calc(y):
 
-oneElem = browser.find_element_by_name('one')
-oneElem.click()
+	browser = webdriver.Firefox()
+	browser.get('http://jeremydomasian.github.io/supercalc_v1-4/SuperCalc-v1.html')
 
-plusElem = browser.find_element_by_name('plus')
-plusElem.click()
+	oneElem = browser.find_element_by_name('one')
+	oneElem.click()
 
-fiveElem = browser.find_element_by_name('five')
-fiveElem.click()
+	plusElem = browser.find_element_by_name('plus')
+	plusElem.click()
 
-equaElem = browser.find_element_by_name('DoIt')
-equaElem.click()
+	fiveElem = browser.find_element_by_name('five')
+	fiveElem.click()
 
-divElem = browser.find_element_by_name('div')
-divElem.click()
+	equaElem = browser.find_element_by_name('DoIt')
+	equaElem.click()
 
-secElem = browser.find_element_by_name('two')
-secElem.click()
+	cleariElem = browser.find_element_by_name('clear')
+	cleariElem.click()
 
-equalElem = browser.find_element_by_name('DoIt')
-equalElem.click()
+	onelElem = browser.find_element_by_name('one')
+	onelElem.click()
 
-subElem = browser.find_element_by_name('minus')
-subElem.click()
+	eightElem = browser.find_element_by_name('eight')
+	eightElem.click()
 
-fourElem = browser.find_element_by_name('four')
-fourElem.click()
+	divElem = browser.find_element_by_name('div')
+	divElem.click()
 
-equatElem = browser.find_element_by_name('DoIt')
-equatElem.click()
+	secElem = browser.find_element_by_name('two')
+	secElem.click()
+
+	equalElem = browser.find_element_by_name('DoIt')
+	equalElem.click()
+
+	clearyElem = browser.find_element_by_name('clear')
+	clearyElem.click()
+
+	threeElem = browser.find_element_by_name('three')
+	threeElem.click()
+
+	subElem = browser.find_element_by_name('minus')
+	subElem.click()
+
+	fourElem = browser.find_element_by_name('four')
+	fourElem.click()
+
+	equatElem = browser.find_element_by_name('DoIt')
+	equatElem.click()
+
+	clearmElem = browser.find_element_by_name('clear')
+	clearmElem.click()
+
+	sevenElem = browser.find_element_by_name('seven')
+	sevenElem.click()
+
+	sixElem = browser.find_element_by_name('six')
+	sixElem.click()
+
+	nineElem = browser.find_element_by_name('nine')
+	nineElem.click()
+
+	multElem = browser.find_element_by_name('times')
+	multElem.click()
+
+	fiveElem = browser.find_element_by_name('five')
+	fiveElem.click()
+
+	zeroElem = browser.find_element_by_name('zero')
+	zeroElem.click()
+
+	equaElem = browser.find_element_by_name('DoIt')
+	equaElem.click()
+
+	clearElem = browser.find_element_by_name('clear')
+	clearElem.click()
